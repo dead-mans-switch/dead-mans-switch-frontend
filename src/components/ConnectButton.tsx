96dbd1b9a5db55ae22bbebe770794e4bf2750358
Raúl R. Pearson <raulrpearson@protonmail.com>
2022-04-25 22:31:29 +0200

    Add voyager link to connect button

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,13 +1,25 @@
-import { Button, Text } from "@chakra-ui/react";
+import { Button, Link } from "@chakra-ui/react";
 import { useStarknet, InjectedConnector } from "@starknet-react/core";
+import NextLink from "next/link";
 
 import truncateAddress from "~/lib/truncateAddress";
+
+// TODO: don't hardcode goerli
 
 export function ConnectButton() {
   const { account, connect } = useStarknet();
 
   if (account) {
-    return <Text textAlign="center">{truncateAddress(account)}</Text>;
+    return (
+      <NextLink
+        href={`https://goerli.voyager.online/contract/${account}`}
+        passHref
+      >
+        <Link textAlign="center" color="gray.50" target="_blank">
+          {truncateAddress(account)}
+        </Link>
+      </NextLink>
+    );
   }
 
   return (
