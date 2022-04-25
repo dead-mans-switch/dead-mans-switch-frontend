bd9171ac4e5834938554e81a5028b34e4e536973
Raúl R. Pearson <raulrpearson@protonmail.com>
2022-04-26 00:48:38 +0200

    Add Layout and app route

---
+++ src/components/Layout.tsx
@@ -0,0 +1,46 @@
+import { Box, Flex, HStack, Link, Text } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { SiGithub, SiTwitter, SiYoutube } from "react-icons/si";
+
+import { ConnectButton } from "~/components/ConnectButton";
+import Logo from "~/components/Logo";
+
+interface LayoutProps {
+  children?: React.ReactNode;
+}
+
+function Navbar() {
+  return (
+    <Flex as="nav" justify="space-between" align="center" mx={6}>
+      <NextLink href="/" passHref>
+        <Link>
+          <Logo height="60" />
+        </Link>
+      </NextLink>
+      <ConnectButton />
+    </Flex>
+  );
+}
+
+function Footer() {
+  return (
+    <Flex as="nav" justify="space-between" w="full" p={6}>
+      <HStack spacing={3}>
+        <SiGithub />
+        <SiTwitter />
+        <SiYoutube />
+      </HStack>
+      <Text align="center">Built with ❤️ on top of Starknet and Argent X</Text>
+    </Flex>
+  );
+}
+
+export default function Layout({ children }: LayoutProps): JSX.Element {
+  return (
+    <Flex direction="column" minH="100vh">
+      <Navbar />
+      <Box flexGrow={1}>{children}</Box>
+      <Footer />
+    </Flex>
+  );
+}
