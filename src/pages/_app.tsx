085942516a45092e4e36913029596142f8375a13
Amaro-Koberle <amaro@amarokoberle.com>
2022-04-25 23:06:31 +0200

    a few styling changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,13 @@
-import type { AppProps } from "next/app";
-import NextHead from "next/head";
-import { InjectedConnector, StarknetProvider } from "@starknet-react/core";
-import {
-  ChakraProvider,
-  extendTheme,
-  withDefaultColorScheme,
-} from "@chakra-ui/react";
+import type { AppProps } from 'next/app';
+import NextHead from 'next/head';
+import { InjectedConnector, StarknetProvider } from '@starknet-react/core';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
-import { Global } from "@emotion/react";
+import { Global } from '@emotion/react';
 
 const Fonts = () => (
-  <Global
-    styles={`
+	<Global
+		styles={`
       @font-face {
         font-family: 'Heading Font Name';
         font-style: normal;
@@ -21,39 +17,34 @@ const Fonts = () => (
         unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
       }
       `}
-  />
+	/>
 );
 
-const theme = extendTheme(
-  withDefaultColorScheme({
-    colorScheme: "red",
-    components: ["Button", "Badge"],
-  }),
-  {
-    styles: {
-      global: {
-        "html, body": {
-          color: "gray.50",
-          backgroundColor: "gray.900",
-        },
-      },
-    },
-  }
-);
+const theme = extendTheme({
+	//	fonts: { heading: 'BabakOne-Regular' },
+	styles: {
+		global: {
+			'html, body': {
+				color: 'gray.50',
+				backgroundColor: 'gray.900',
+			},
+		},
+	},
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const connectors = [new InjectedConnector()];
+	const connectors = [new InjectedConnector()];
 
-  return (
-    <StarknetProvider autoConnect connectors={connectors}>
-      <ChakraProvider theme={theme}>
-        <NextHead>
-          <title>StarkNet ❤️ React</title>
-        </NextHead>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </StarknetProvider>
-  );
+	return (
+		<StarknetProvider autoConnect connectors={connectors}>
+			<ChakraProvider theme={theme}>
+				<NextHead>
+					<title>Dead Man's Switch</title>
+				</NextHead>
+				<Component {...pageProps} />
+			</ChakraProvider>
+		</StarknetProvider>
+	);
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,154 +1,161 @@
 import {
-  Box,
-  Button,
-  Flex,
-  Heading,
-  Text,
-  Image,
-  VStack,
-  HStack,
-} from "@chakra-ui/react";
-import { SiGithub, SiTwitter, SiYoutube } from "react-icons/si";
-import Logo from "~/components/Logo";
-import { ConnectButton } from "~/components/ConnectButton";
+	Box,
+	Button,
+	Flex,
+	Heading,
+	Text,
+	Image,
+	VStack,
+	HStack,
+} from '@chakra-ui/react';
+import { SiGithub, SiTwitter, SiYoutube } from 'react-icons/si';
+import Logo from '~/components/Logo';
+import { ConnectButton } from '~/components/ConnectButton';
 
 function Navbar() {
-  return (
-    <Flex as="nav" justify="space-between" align="center" mx={6}>
-      <Logo height="60" />
-      <ConnectButton />
-    </Flex>
-  );
+	return (
+		<Flex as='nav' justify='space-between' align='center' mx={6}>
+			<Logo height='60' />
+			<ConnectButton />
+		</Flex>
+	);
 }
 function Footer() {
-  return (
-    <Flex as="nav" justify="space-between" w="full" p={6}>
-      <HStack spacing={3}>
-        <SiGithub />
-        <SiTwitter />
-        <SiYoutube />
-      </HStack>
-      <Text align="center">Built with ❤️ on top of Starknet and Argent X</Text>
-    </Flex>
-  );
+	return (
+		<Flex as='nav' justify='space-between' w='full' p={6}>
+			<HStack spacing={3}>
+				<SiGithub />
+				<SiTwitter />
+				<SiYoutube />
+			</HStack>
+			<Text align='center'>Built with ❤️ on top of Starknet and Argent X</Text>
+		</Flex>
+	);
 }
 
 interface TutorialCardProps {
-  children: string;
-  imageSource: string;
-  stepNumber: number;
+	children: string;
+	imageSource: string;
+	stepNumber: number;
 }
 function TutorialCard(props: TutorialCardProps) {
-  return (
-    <Box
-      position="relative"
-      bg="gray.700"
-      p={3}
-      borderRadius="2xl"
-      w="full"
-      minW={60}
-      maxW={100}
-      padding={0}
-    >
-      <Heading
-        position="absolute"
-        top={3}
-        left={3}
-        size="3xl"
-        color="red.700"
-        opacity="0.75"
-      >
-        {props.stepNumber}
-      </Heading>
-      <Image w="full" borderTopRadius="2xl" h={40} src={props.imageSource} />
-      <Text m={3}>{props.children}</Text>
-    </Box>
-  );
+	return (
+		<Box
+			position='relative'
+			bg='gray.700'
+			p={3}
+			borderRadius='2xl'
+			w='full'
+			minW={60}
+			maxW={100}
+			padding={0}>
+			<Heading
+				position='absolute'
+				top={3}
+				left={3}
+				size='3xl'
+				color='red.700'
+				opacity='0.75'>
+				{props.stepNumber}
+			</Heading>
+			<Image w='full' borderTopRadius='2xl' h={40} src={props.imageSource} />
+			<Text m={3}>{props.children}</Text>
+		</Box>
+	);
 }
 
 export default function Index() {
-  return (
-    <VStack>
-      <Box as="header" m={6} w="full">
-        <Navbar />
-        <VStack mx={6}>
-          <HStack mt={6}>
-            <Logo height="150" />
-            <Heading as="h1" size="4xl">
-              Dead Man's Switch
-            </Heading>
-          </HStack>
-          <Flex gap={6} mt={6} wrap="wrap" align="center">
-            <Button h={16}>Set dead man's switch</Button>
-            <Heading as="h4" size="sm">
-              Ensure that your loved ones get access to your assets <br /> in
-              case of your death.
-            </Heading>
-          </Flex>
-        </VStack>
-      </Box>
-      <Box
-        as="main"
-        bg="gray.800"
-        p={6}
-        maxW="xl"
-        m={6}
-        mt={40}
-        borderRadius="3xl"
-      >
-        <VStack>
-          <Heading as="h3" size="md" textAlign="center">
-            What is Dead Man's Switch?
-          </Heading>
-          <Text maxW="xl" mt={3} color="text">
-            Dead Man's Switch allows trusted accounts to access the assets in
-            your wallet in case your wallet has not been used for a long period
-            of time. You decide who gets your inheritance and how much much time
-            has to elapse before the dead man's switch triggers.
-          </Text>
-        </VStack>
-        <VStack>
-          <Heading as="h3" size="md" mt={6} textAlign="center">
-            How does it work?
-          </Heading>
-          <Flex gap={3} wrap="wrap">
-            <TutorialCard
-              imageSource="./DeadMansSwitch_Step1.jpg"
-              stepNumber={1}
-            >
-              Connect your wallet.
-            </TutorialCard>
-            <TutorialCard
-              imageSource="./DeadMansSwitch_Step2.jpg"
-              stepNumber={2}
-            >
-              Set your trusted address and the timer.
-            </TutorialCard>
-            <TutorialCard
-              imageSource="./DeadMansSwitch_Step3.jpg"
-              stepNumber={3}
-            >
-              Die.
-            </TutorialCard>
-            <TutorialCard
-              imageSource="./DeadMansSwitch_Step4.jpg"
-              stepNumber={4}
-            >
-              Your trusted address will be able to access your assets after the
-              timer elapses.
-            </TutorialCard>
-          </Flex>
-        </VStack>
-        <VStack w="full">
-          <Heading as="h3" size="md" mt={8} textAlign="center">
-            Get started
-          </Heading>
-          <Button w="full" mt={3} h={16}>
-            Set dead man's switch
-          </Button>
-        </VStack>
-      </Box>
-      <Footer />
-    </VStack>
-  );
+	return (
+		<VStack>
+			<Box as='header' m={6} w='full'>
+				<Navbar />
+				<VStack mx={6}>
+					<HStack mt={6}>
+						<Logo height='150' />
+						<Heading
+							as='h1'
+							size='4xl'
+							bgClip='text'
+							bgGradient='linear(to-br, #f79263, #e24b70)'>
+							Dead Man's Switch
+						</Heading>
+					</HStack>
+					<Flex gap={6} mt={6} wrap='wrap' align='center'>
+						<Button h={16} bgGradient='linear(to-br, #f79263, #e24b70)'>
+							Set dead man's switch
+						</Button>
+						<Heading
+							bgGradient='linear(to-br, #f79263, #e24b70)'
+							as='h4'
+							size='sm'
+							bgClip='text'>
+							Ensure that your loved ones get access to your assets <br /> in
+							case of your death.
+						</Heading>
+					</Flex>
+				</VStack>
+			</Box>
+			<Box
+				as='main'
+				bg='gray.800'
+				p={6}
+				maxW='xl'
+				m={6}
+				mt={40}
+				borderRadius='3xl'>
+				<VStack>
+					<Heading as='h3' size='md' textAlign='center'>
+						What is Dead Man's Switch?
+					</Heading>
+					<Text maxW='xl' mt={3} color='text'>
+						Dead Man's Switch allows trusted accounts to access the assets in
+						your wallet in case your wallet has not been used for a long period
+						of time. You decide who gets your inheritance and how much much time
+						has to elapse before the dead man's switch triggers.
+					</Text>
+				</VStack>
+				<VStack>
+					<Heading as='h3' size='md' mt={6} textAlign='center'>
+						How does it work?
+					</Heading>
+					<Flex gap={3} wrap='wrap'>
+						<TutorialCard
+							imageSource='./DeadMansSwitch_Step1.jpg'
+							stepNumber={1}>
+							Connect your wallet.
+						</TutorialCard>
+						<TutorialCard
+							imageSource='./DeadMansSwitch_Step2.jpg'
+							stepNumber={2}>
+							Set your trusted address and the timer.
+						</TutorialCard>
+						<TutorialCard
+							imageSource='./DeadMansSwitch_Step3.jpg'
+							stepNumber={3}>
+							Die.
+						</TutorialCard>
+						<TutorialCard
+							imageSource='./DeadMansSwitch_Step4.jpg'
+							stepNumber={4}>
+							Your trusted address will be able to access your assets after the
+							timer elapses.
+						</TutorialCard>
+					</Flex>
+				</VStack>
+				<VStack w='full'>
+					<Heading as='h3' size='md' mt={8} textAlign='center'>
+						Get started
+					</Heading>
+					<Button
+						w='full'
+						mt={3}
+						h={16}
+						bgGradient='linear(to-br, #f79263, #e24b70)'>
+						Set dead man's switch
+					</Button>
+				</VStack>
+			</Box>
+			<Footer />
+		</VStack>
+	);
 }
