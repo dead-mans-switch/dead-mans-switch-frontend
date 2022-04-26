de1ccd3a9cd9b5339555334b6c02ed39bfb85d87
Amaro-Koberle <amaro@amarokoberle.com>
2022-04-26 07:02:12 +0200

    fixed tab styling

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,50 +1,68 @@
 import {
-  Flex,
-  Tabs,
-  TabList,
-  TabPanels,
-  Tab,
-  TabPanel,
-  Text,
-} from "@chakra-ui/react";
-import { useStarknet } from "@starknet-react/core";
-import * as React from "react";
+	Flex,
+	Tabs,
+	TabList,
+	TabPanels,
+	Tab,
+	TabPanel,
+	Text,
+} from '@chakra-ui/react';
+import { useStarknet } from '@starknet-react/core';
+import * as React from 'react';
 
-import Layout from "~/components/Layout";
-import Manage from "~/components/Manage";
-import Redeem from "~/components/Redeem";
+import Layout from '~/components/Layout';
+import Manage from '~/components/Manage';
+import Redeem from '~/components/Redeem';
 
 export default function App(): JSX.Element {
-  const { account } = useStarknet();
+	const { account } = useStarknet();
 
-  if (account) {
-    return (
-      <Layout>
-        <Flex direction="column" gap={8} minW="lg" maxW="xl" m="0 auto">
-          <Tabs isFitted>
-            <TabList>
-              <Tab>Manage switch</Tab>
-              <Tab>Redeem inheritance</Tab>
-            </TabList>
-            <TabPanels>
-              <TabPanel>
-                <Manage />
-              </TabPanel>
-              <TabPanel>
-                <Redeem />
-              </TabPanel>
-            </TabPanels>
-          </Tabs>
-        </Flex>
-      </Layout>
-    );
-  }
+	if (account) {
+		return (
+			<Layout>
+				<Flex direction='column' gap={8} minW='lg' maxW='xl' m='0 auto'>
+					<Tabs isFitted variant='soft-rounded'>
+						<TabList>
+							<Tab
+								bg='gray.800'
+								color='white'
+								_selected={{
+									color: 'gray.900',
+									bgGradient: 'linear(to-br, #f79263, #e24b70)',
+								}}
+								mx={2}>
+								Manage switch
+							</Tab>
+							<Tab
+								bg='gray.800'
+								color='white'
+								_selected={{
+									color: 'gray.900',
+									bgGradient: 'linear(to-br, #f79263, #e24b70)',
+								}}
+								mx={2}>
+								Redeem inheritance
+							</Tab>
+						</TabList>
+						<TabPanels>
+							<TabPanel>
+								<Manage />
+							</TabPanel>
+							<TabPanel>
+								<Redeem />
+							</TabPanel>
+						</TabPanels>
+					</Tabs>
+				</Flex>
+			</Layout>
+		);
+	}
 
-  return (
-    <Layout>
-      <Text fontSize="xl" fontWeight="600">
-        Connect to access the app
-      </Text>
-    </Layout>
-  );
+	return (
+		<Layout>
+			<Text fontSize='xl' fontWeight='600'>
+				Connect to access the app
+			</Text>
+		</Layout>
+	);
 }
